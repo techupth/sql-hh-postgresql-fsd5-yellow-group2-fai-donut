4c9a2b05d27ed7dc16474c6c1f56a898283aa1a6
ณัฐพร นาคพงษ์ <Missterdonat33@gmail.com>
2024-01-22 16:34:33 +0700

    feat : postgresql database e-commerce

--- e-commerce-tables.sql
+++ e-commerce-tables.sql
@@ -1,1 +1,30 @@
 -- Start writing code here
+
+CREATE TABLE users (
+  user_id INT UNIQUE PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+  username VARCHAR ( 255 ) UNIQUE NOT NULL,
+  email  VARCHAR ( 255 ) UNIQUE NOT NULL,
+  password VARCHAR ( 255 ) NOT NULL,
+  created_at TIMESTAMPTZ UNIQUE NOT NULL
+);
+
+CREATE TABLE products (
+  product_id  INT UNIQUE PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+  name VARCHAR ( 255 ) NOT NULL,
+  description  TEXT NULL,
+  price NUMERIC(10,2) NOT NULL,
+  created_at TIMESTAMPTZ UNIQUE NOT NULL
+);
+
+CREATE TABLE categories (
+  category_id INT UNIQUE PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+  name VARCHAR ( 255 ) NOT NULL,
+  created_at TIMESTAMPTZ UNIQUE NOT NULL
+);
+
+CREATE TABLE product_categories (
+  product_category_id   INT UNIQUE PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+  product_id INT REFERENCES products(product_id) ON DELETE CASCADE,
+  category_id INT REFERENCES categories(category_id) ON DELETE CASCADE,
+  created_at TIMESTAMPTZ UNIQUE NOT NULL
+);
